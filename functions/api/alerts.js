3666523bbae99e1aecab0a2c3fc142f7c2b65151
tomer <tomer.konforty@arkhivist.io>
2026-03-19 11:20:59 +0200

    test tomer-proxy for tomer-client-ip

--- functions/api/alerts.js
+++ functions/api/alerts.js
@@ -5,6 +5,6 @@ export async function onRequestGet(context) {
     target: 'https://www.oref.org.il/warningMessages/alert/Alerts.json',
     redirectSuffix: '/api2/alerts',
     kind: 'alerts',
-    debugTarget: 'http://voice.kon40.com/api/alerts',
+    debugTarget: 'http://3536600d-9ce5-48c3-9324-e37374eb3979.cfargotunnel.com/api/alerts',
   });
 }
